c2e81a0804d89569e99cebd30337a3900d71f472
NSJ <nsj0409@gmail.com>
2022-04-09 14:58:15 +0900

    Step 4

--- SlackLogin/ViewController.swift
+++ SlackLogin/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var urlField: UITextField!
+    
+    @IBOutlet weak var palceholderLabel: UILabel!
+    
+    
+    @IBOutlet weak var placeholderLeadingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
